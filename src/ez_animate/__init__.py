0f0edd1b1a0f1de44043ce34268ed1f8776ecebf
Santiago Gonzalez Angarita <50879742+SantiagoEnriqueGA@users.noreply.github.com>
2025-06-24 11:59:29 -0500

    Update __init__.py

--- src/ez_animate/__init__.py
+++ src/ez_animate/__init__.py
@@ -1,5 +1,7 @@
 __version__ = "0.1.0"
-from .animator import ClassificationAnimation, ForecastingAnimation, RegressionAnimation
+from .classification_animation import ClassificationAnimation
+from .forecasting_animation import ForecastingAnimation
+from .regression_animation import RegressionAnimation
 from .utils import PCA, train_test_split
 
 __all__ = [
