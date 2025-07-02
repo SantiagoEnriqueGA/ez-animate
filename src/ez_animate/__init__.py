6dc491e215f8084fd50af7783f7f5a152db8008f
Santiago Gonzalez Angarita <50879742+SantiagoEnriqueGA@users.noreply.github.com>
2025-07-02 14:31:22 -0500

    feat: add ClusteringAnimation class for visualizing clustering algorithms

--- src/ez_animate/__init__.py
+++ src/ez_animate/__init__.py
@@ -1,5 +1,6 @@
 __version__ = "0.1.0"
 from .classification_animation import ClassificationAnimation
+from .clustering_animation import ClusteringAnimation
 from .forecasting_animation import ForecastingAnimation
 from .regression_animation import RegressionAnimation
 from .utils import PCA, train_test_split
@@ -8,6 +9,7 @@ __all__ = [
     "ForecastingAnimation",
     "RegressionAnimation",
     "ClassificationAnimation",
+    "ClusteringAnimation",
     "PCA",
     "train_test_split",
 ]
